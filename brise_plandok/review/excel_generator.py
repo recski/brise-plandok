cdbea0a7e6ad2ebdbab97cfc8fa47dbcf10fbcb7
Eszti <eszter.iklodi@gmail.com>
2021-08-26 17:32:46 +0200

    Add sentence review column

--- brise_plandok/review/excel_generator.py
+++ brise_plandok/review/excel_generator.py
@@ -7,12 +7,17 @@ from openpyxl.styles import Alignment, Font
 from openpyxl.worksheet.datavalidation import DataValidation
 from brise_plandok.annotation.attributes import ATTR_TO_CAT
 
+review_sheet_name = "Review"
+attribute_named_range = "Attribute"
+attribute_review_named_range = "Attribute_Review"
+sentence_review_named_range = "Sentence_Review"
 
 class ExcelGenerator:
 
     def __init__(self) -> None:
         self.output = os.path.join(os.path.dirname(__file__), "review.xlsx")
         self.attribute_offset = 4
+        self.first_attribute_column = 4
 
     def generate_review_excel(self, merged_annotations):
         excel_template = os.path.join(
@@ -22,26 +27,22 @@ class ExcelGenerator:
         self._save_workbook(workbook)
 
     def _fill_workbook(self, workbook, merged_annotations):
-        review_sheet = workbook["Review"]
+        review_sheet = workbook[review_sheet_name]
         row = 2
         for sen_id, annotation in merged_annotations.items():
-            self.__fill_sens(sen_id, review_sheet, row, annotation)
+            self.__fill_sentences(sen_id, review_sheet, row, annotation)
             self.__fill_attributes(annotation, review_sheet, row)
             row += 1
         self.__add_validation(review_sheet)
 
-    def __fill_sens(self, sen_id, review_sheet, row, annotation):
-        sen_id_cell = "A"+str(row)
-        sen_txt_cell = "B"+str(row)
-        review_sheet[sen_id_cell] = sen_id
-        review_sheet[sen_txt_cell] = annotation["text"]
-        review_sheet[sen_id_cell].alignment = Alignment(wrapText=True)
-        review_sheet[sen_txt_cell].alignment = Alignment(wrapText=True)
-        review_sheet[sen_id_cell].font = Font(size=12)
-        review_sheet[sen_txt_cell].font = Font(size=12)
+    def __fill_sentences(self, sen_id, review_sheet, row, annotation):
+        review_sheet.cell(row=row, column=1).value = sen_id
+        review_sheet.cell(row=row, column=3).value = annotation["text"]
+        review_sheet.cell(row=row, column=3).alignment = Alignment(wrapText=True)
+        review_sheet.cell(row=row, column=3).font = Font(size=12)
 
     def __fill_attributes(self, annotation, review_sheet, row):
-        base_column = ord('C')
+        col = self.first_attribute_column
         for attribute_name, attribute_props in annotation["attributes"].items():
             attribute_name = self.__normalize_attr_names(attribute_name)
             if attribute_name not in ATTR_TO_CAT:
@@ -49,19 +50,14 @@ class ExcelGenerator:
                     f"\"{attribute_name}\" does not belong to any category - will be ignored")
             else:
                 self.__fill_attribute(
-                    attribute_name, attribute_props, review_sheet, base_column, row)
-                base_column += self.attribute_offset
+                    attribute_name, attribute_props, review_sheet, col, row)
+                col += self.attribute_offset
 
-    def __fill_attribute(self, attribute_name, attribute_props, review_sheet, base_column, row):
-        review_sheet[self.__get_cell(
-            base_column, 0, row)] = ATTR_TO_CAT[attribute_name]
-        review_sheet[self.__get_cell(base_column, 1, row)] = attribute_name
-        review_sheet[self.__get_cell(
-            base_column, 2, row)] = attribute_props["count"]
-        review_sheet[self.__get_cell(base_column, 3, row)] = "OK"
-
-    def __get_cell(self, base_column, column_offset, row):
-        return chr(base_column + column_offset)+str(row)
+    def __fill_attribute(self, attribute_name, attribute_props, review_sheet, col, row):
+        review_sheet.cell(row=row, column=col).value = ATTR_TO_CAT[attribute_name]
+        review_sheet.cell(row=row, column=col+1).value = attribute_name
+        review_sheet.cell(row=row, column=col+2).value = attribute_props["count"]
+        review_sheet.cell(row=row, column=col+3).value = "OK"
 
     def __normalize_attr_names(self, attribute_name):
         if attribute_name == "Verkehrsflaeche_ID":
@@ -69,36 +65,42 @@ class ExcelGenerator:
         return attribute_name
 
     def __add_validation(self, review_sheet):
-        data_val = DataValidation(type="list", formula1='=Attribute')
-        review_val = DataValidation(type="list", formula1='=Review')
+        data_val = DataValidation(type="list", formula1=f"={attribute_named_range}")
         review_sheet.add_data_validation(data_val)
+        review_val = DataValidation(type="list", formula1=f"={attribute_review_named_range}")
         review_sheet.add_data_validation(review_val)
+        sen_review_val = DataValidation(type="list", formula1=f"={sentence_review_named_range}")
+        review_sheet.add_data_validation(sen_review_val)
         for row in range(2, review_sheet.max_row):
             self.__add_validation_for_row(
-                review_sheet, row, data_val, review_val)
+                review_sheet, row, data_val, review_val, sen_review_val)
 
-    def __add_validation_for_row(self, review_sheet, row, data_val, review_val):
+    def __add_validation_for_row(self, review_sheet, row, data_val, review_val, sen_review_val):
         for cell in review_sheet[str(row)]:
             col = cell.column
-            if self.__not_attribute(col):
-                continue
-            self.__add_validation_for_cell(review_sheet, row, col, data_val, review_val)
+            if self.__is_sentence_review_cell(col):
+                self.__add_validation_for_sentence_review(review_sheet, row, col, sen_review_val)
+            elif self.__is_category_cell(col):
+                self.__add_validations_for_attribute(
+                    review_sheet, row, col, data_val, review_val)
 
-    def __add_validation_for_cell(self, review_sheet, row, col, data_val, review_val):
-        if self.__is_category_cell(col):
-            data_val.add(review_sheet.cell(row=row, column=col))
-            sub_data_val = DataValidation(
-                type="list", formula1='==INDIRECT(${0}${1})'.format(get_column_letter(col), str(row)))
-            review_sheet.add_data_validation(sub_data_val)
-            sub_data_val.add(review_sheet.cell(row=row, column=col+1))
-            review_val.add(
-                review_sheet.cell(row=row, column=col+3))
+    def __add_validation_for_sentence_review(self, review_sheet, row, col, sentence_review_val):
+        sentence_review_val.add(review_sheet.cell(row=row, column=col))
 
-    def __not_attribute(self, col):
-        return col < 3
+    def __add_validations_for_attribute(self, review_sheet, row, col, data_val, review_val):
+        data_val.add(review_sheet.cell(row=row, column=col))
+        sub_data_val = DataValidation(
+            type="list", formula1='==INDIRECT(${0}${1})'.format(get_column_letter(col), str(row)))
+        review_sheet.add_data_validation(sub_data_val)
+        sub_data_val.add(review_sheet.cell(row=row, column=col+1))
+        review_val.add(
+            review_sheet.cell(row=row, column=col+3))
+
+    def __is_sentence_review_cell(self, col):
+        return col == 2
 
     def __is_category_cell(self, col):
-        return (col - 3) % self.attribute_offset == 0
+        return (col - self.first_attribute_column) % self.attribute_offset == 0
 
     def _save_workbook(self, workbook):
         workbook.save(self.output)

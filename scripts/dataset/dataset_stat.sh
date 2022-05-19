6e975529dbe537671aec96d76c909ca0c7b6976f
Eszti <eszter.iklodi@gmail.com>
2022-05-19 21:59:05 +0200

    Correct stats

--- scripts/dataset/dataset_stat.sh
+++ scripts/dataset/dataset_stat.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+echo "total"
+
+echo "Number of docs"
+cat data/*/*.json | jq '.id' | wc -l
+
+echo "Number of sentences"
+cat data/*/*.json | jq '.sens[].id' | wc -l
+
+echo "Number of sentences with gold attributes"
+cat data/*/*.json | jq ".sens[] | select(.segmentation_error == false) | select(.gold_attributes != {}) | .id" | wc -l
+
+echo "Sum of all gold attributes"
+cat data/*/*.json | jq '.sens[] | select(.segmentation_error == false) | .gold_attributes | keys[]' | wc -l
+
+echo "Gold attribute distribution"
+cat data/*/*.json | jq '.sens[] | select(.segmentation_error == false) | .gold_attributes | keys[]' | sort | uniq -c | sort -nr
+
+echo ""
+
 datasets=("train" "valid" "test")
 
 for dataset_name in "${datasets[@]}"; do
@@ -12,14 +31,14 @@ for dataset_name in "${datasets[@]}"; do
     echo "Number of sentences"
     cat data/"$dataset_name"/*.json | jq '.sens[].id' | wc -l
     
-    echo "Number of sentences with attributes"
-    cat data/"$dataset_name"/*.json | jq ".sens[] | select(.full_annotated_attributes.attributes != null) | select(.full_annotated_attributes != {}) | select(.full_annotated_attributes | keys | index(\"DON'T ANNOTATE THIS SENTENCE\") | not) | .id" | wc -l
+    echo "Number of sentences with gold attributes"
+    cat data/"$dataset_name"/*.json | jq ".sens[] | select(.segmentation_error == false) | select(.gold_attributes != {}) | .id" | wc -l
     
     echo "Sum of all gold attributes"
-    cat data/"$dataset_name"/*.json | jq ".sens[] | select(.full_annotated_attributes.attributes != null) | select(.full_annotated_attributes | keys | index(\"DON'T ANNOTATE THIS SENTENCE\") | not) | .full_annotated_attributes.attributes" | jq 'keys[]' | wc -l
+    cat data/"$dataset_name"/*.json | jq '.sens[] | select(.segmentation_error == false) | .gold_attributes | keys[]' | wc -l
     
     echo "Gold attribute distribution"
-    cat data/"$dataset_name"/*.json | jq 'select(.full_gold == true)' | jq '.sens[].gold_attributes | keys[]' | sort | uniq -c | sort -nr
+    cat data/"$dataset_name"/*.json | jq '.sens[] | select(.segmentation_error == false) | .gold_attributes | keys[]' | sort | uniq -c | sort -nr
     
     echo ""
 done
